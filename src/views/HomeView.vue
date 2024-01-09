d784f9f7051dc9c891d2d51549dae2260cd4f090
Roman <roman@Romans-MacBook-Pro.local>
2024-01-09 20:14:20 +0200

    [WL] Implemented correct number of tiles rendering

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,35 +1,60 @@
 <script setup lang="ts">
+import { ref, computed } from 'vue';
 import { makeHttpRequest, API_ROOT, API_KEY } from '@/utils/httpUtils';
 import { store, setGalleryData, resetGalleryData } from '@/utils/store';
 import Button from '@/components/ButtonVariant.vue';
 import ArtGallery from '@/components/ArtGallery.vue';
 
-import { ref, computed } from 'vue';
+import type { GalleryStoreT } from '@/utils/types';
  
-const page = ref(1);
+const loadingMoreSteps = ref(2);
+
+const visibleItems = ref<GalleryStoreT>({
+  count: 0,
+  artObjects: {},
+  artObjectsKeys: [],
+});
+
+const isPending = ref(false);
 
 const search = defineModel();
 
+const itemsPerPage = 12;
+
 const fetch = async () => {
+  const nextPage = store.artObjectsKeys.length / 20;
+
+  isPending.value = true;
+
   const response = await makeHttpRequest({ 
-    url: `${API_ROOT}/collection?key=${API_KEY}&ps=12&p=${page.value}&q=${search.value}&imgonly=true`,
+    url: `${API_ROOT}/collection?key=${API_KEY}&ps=20&p=${nextPage}&q=${search.value}&imgonly=true`,
   });
 
   setGalleryData(response);
+
+  updateSlice();
+
+  isPending.value = false;
 };
 
 const handleSubmit = () => {
   resetGalleryData();
 
-  page.value = 1;
+  loadingMoreSteps.value = 1;
 
   fetch();
 }
 
-const loadingMore = () => {
-  page.value++;
+const updateSlice = () => {
+  const start = (loadingMoreSteps.value - 1) * itemsPerPage;
 
-  fetch();
+  visibleItems.value = {
+    ...store,
+    artObjectsKeys: [
+      ...visibleItems.value.artObjectsKeys,
+      ...store.artObjectsKeys.slice(start, start + itemsPerPage)
+    ],
+  };
 };
 
 const shouldLoadMore = computed(() => {
@@ -37,6 +62,16 @@ const shouldLoadMore = computed(() => {
 
   return artObjectsKeys.length < count;
 });
+
+const loadingMore = () => {
+  if (loadingMoreSteps.value * itemsPerPage >= store.artObjectsKeys.length) {
+    fetch();
+  } else {
+    updateSlice();
+  }
+
+  loadingMoreSteps.value++;
+};
 </script>
 
 <template>
@@ -52,10 +87,11 @@ const shouldLoadMore = computed(() => {
       <Button type="submit">Search</Button>
     </form>
 
-    <ArtGallery :gallery="store" />
+    <ArtGallery :gallery="visibleItems" />
 
     <div class="more-container">
       <Button 
+        :disabled="isPending"
         v-if="shouldLoadMore"
         type="button"
         @click="loadingMore" 
